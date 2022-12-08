89a288a2b66f436df8ba7e9445e394195072a15b
Ismael avotrasoa <ismaelavotrasoa@Ismaels-MacBook-Pro.local>
2022-12-08 15:09:58 +0100

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,12 +14,12 @@ const Header = () => {
           <img src={Logo} alt='logo' />
         </Link>
         <div className='flex items-center gap-6'>
-          <Link className='hover:text-violet-900 transition' to=''>
+          <Link className='hover:text-violet-900 transition' to='/'>
             Log in
           </Link>
           <Link
             className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition '
-            to=''
+            to='/'
           >
             Sign up
           </Link>
